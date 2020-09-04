732dafb5dc6ed10ae0863f172b89abdb4f0f6dd0
Eugene Ciurana <eugenex@cime.net>
2020-09-04 11:17:34 -0700

    Unit tests and basic implementation complete

--- poof/__init__.py
+++ poof/__init__.py
@@ -1,3 +1,45 @@
 # See: https://github.com/pr3d4t0r/poof/blob/master/LICENSE.txt
 # vim: set fileencoding=utf-8:
 
+
+import argparse
+
+
+# *** constants ***
+
+VALID_COMMANDS = ( 'upload', 'download', 'verify', 'test' )
+
+
+# *** functions ***
+
+def _parseCLI():
+    config = dict()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('command', type=str, help = '|'.join(VALID_COMMANDS))
+
+    args = parser.parse_args()
+    
+    config['command'] = args.command
+
+    if config['command'] not in VALID_COMMANDS:
+        # TODO: Define a cleaner exit here.
+        raise NotImplementedError
+
+    return config
+
+
+def main():
+    config = _parseCLI()
+
+    if config['command'] == 'test':
+        return True
+
+    # Implementation goes here.
+
+
+# *** main ***
+
+if '__main__' == __name__:
+    main()
+
--- tests/test_poof.py
+++ tests/test_poof.py
@@ -2,5 +2,40 @@
 # vim: set fileencoding=utf-8:
 
 
-def test_version():
-    assert 1 == 1
+from poof import _parseCLI
+from poof import main
+
+import copy
+import sys
+
+import pytest
+
+
+# *** pre-test ***
+
+realArgs = copy.deepcopy(sys.argv)
+del sys.argv[1:]
+sys.argv.append('test')
+
+
+# *** tests ***
+
+def test__parseCLI():
+    config = _parseCLI()
+    assert config['command'] == 'test'
+
+    del(sys.argv[1])
+
+    sys.argv.append('bogus')
+
+    with pytest.raises(NotImplementedError):
+        config = _parseCLI()
+
+    del(sys.argv[1])
+
+    sys.argv.append('test')
+
+
+def test_main():
+    assert main() == True
+
